a16b6a80728164086716562ad18bab55bdcccf07
ivanarganda <ivanartista96@gmail.com>
2024-05-10 19:36:43 +0200

    changed api url

--- src/hooks/state/useUrl.jsx
+++ src/hooks/state/useUrl.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function useUrl(type) {
-    const API_URL = 'https://ws-shortlinks.onrender.com';
+    const API_URL = 'https://ws-shortlinks-u8u4.onrender.com';
     // const API_URL = "http://localhost:3000";
     const URLS = {
         'api':API_URL,
